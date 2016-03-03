39f1deefb504c338974809169055e4f84c621f17
schrismartin <schrismartin@me.com>
2016-03-03 11:55:33 -0500

    initial angular commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jassemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jassemApp
+ */
+angular.module('jassemApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jassemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jassemApp
+ */
+angular.module('jassemApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
